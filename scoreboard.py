6c5f02f91160838f7de3747711c110a9bdaedb22
Jamie Flynt <jamflynt@gmail.com>
2022-09-08 22:46:29 -0400

    Completed game.

---
+++ scoreboard.py
@@ -0,0 +1,28 @@
+from turtle import Turtle
+ALIGNMENT = "center"
+FONT = ('Courier', 24, 'normal')
+
+
+class Score(Turtle):
+
+    def __init__(self):
+        super().__init__()
+        print("creating scoreboard")
+        self.score = 0
+        self.color("white")
+        self.penup()
+        self.goto(0, 270)
+        self.hideturtle()
+        self.update_scoreboard()
+
+    def update_scoreboard(self):
+        self.write(f"Score: {self.score}", move=False, align=ALIGNMENT, font=FONT)
+
+    def game_over(self):
+        self.goto(0, 0)
+        self.write("Game Over.", move=False, align=ALIGNMENT, font=FONT)
+
+    def increase_score(self):
+        self.score += 1
+        self.clear()
+        self.update_scoreboard()
