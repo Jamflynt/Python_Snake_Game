f9c4f4f5a9b89179ab7b5e01358e76c41991f9ac
Jamie Flynt <jamflynt@gmail.com>
2022-09-12 22:15:05 -0400

    Added inital High Score to game. High score cannot yet be stored and will reset after program is closed.

--- scoreboard.py
+++ scoreboard.py
@@ -9,6 +9,7 @@ class Score(Turtle):
         super().__init__()
         print("creating scoreboard")
         self.score = 0
+        self.high_score = 0
         self.color("white")
         self.penup()
         self.goto(0, 270)
@@ -16,13 +17,15 @@ class Score(Turtle):
         self.update_scoreboard()
 
     def update_scoreboard(self):
-        self.write(f"Score: {self.score}", move=False, align=ALIGNMENT, font=FONT)
+        self.clear()
+        self.write(f"Score: {self.score} High Score: {self.high_score}", move=False, align=ALIGNMENT, font=FONT)
 
-    def game_over(self):
-        self.goto(0, 0)
-        self.write("Game Over.", move=False, align=ALIGNMENT, font=FONT)
+    def reset(self):
+        if self.score > self.high_score:
+            self.high_score = self.score
+        self.score = 0
+        self.update_scoreboard()
 
     def increase_score(self):
         self.score += 1
-        self.clear()
         self.update_scoreboard()
